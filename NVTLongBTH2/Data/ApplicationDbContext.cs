af6ac15508f6e3bc3c2e83ed2093815aa4478fef
Nguyễn Vũ Trường Long <nguyenvutruonglong06012001@gmail.com>
2022-10-26 11:15:26 +0700

    Demo1
    Demo

---
+++ NVTLongBTH2/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using NVTLongBTH2.Models;
+using Microsoft.EntityFrameworkCore;
+namespace NVTLongBTH2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students {get; set;}
+    }
+}
---
+++ NVTLongBTH2/Models/Student.cs
@@ -0,0 +1,8 @@
+namespace NVTLongBTH2.Models
+{
+    public class Student
+    {
+        public string StudentID {get; set;}
+        public string StudentName {get; set;}
+    }
+}
