86af61ee62ab7a42b6edeb52adcca4ae5c83b2db
Nguyễn Vũ Trường Long <nguyenvutruonglong06012001@gmail.com>
2022-10-26 12:21:51 +0700

    Demo
    Demo 2

---
+++ NVTLongBTH2/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+using NVTLongBTH2.Data;
+using NVTLongBTH2.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+namespace NVTLongBTH2.controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public StudentController (ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var model = await _context.Students.ToListAsync();
+            return View(model);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Student std)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(std);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+        }
+    }
+}
