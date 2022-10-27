051306ce58fe3833cc95bf0378e701f05d30109f
Nguyễn Vũ Trường Long <nguyenvutruonglong06012001@gmail.com>
2022-10-27 09:37:38 +0700

    Demo 3

--- NVTLongBTH2/Controllers/StudentController.cs
+++ NVTLongBTH2/Controllers/StudentController.cs
@@ -2,6 +2,7 @@ using NVTLongBTH2.Data;
 using NVTLongBTH2.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+
 namespace NVTLongBTH2.controllers
 {
     public class StudentController : Controller
@@ -29,6 +30,78 @@ namespace NVTLongBTH2.controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+            return View(std);
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("StudentID, StudentName")] Student std)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students.FindAsync(id);
+            if(student == null)
+            {
+                return NotFound();
+            }
+            if(id != std.StudentID)
+            {
+                return NotFound();
+            }
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(std);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if(!StudentExists(std.StudentID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(std);
+        }   
+        
+        public async Task<IActionResult> Delete(string id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var std = await _context.Students.FirstOrDefaultAsync(m => m.StudentID == id);
+            if (std == null)
+            {
+                return NotFound();
+            }
+            return View(std);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var std = await _context.Students.FindAsync(id);
+            _context.Students.Remove(std);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        private bool StudentExists(string id)
+        {
+            return _context.Students.Any(e => e.StudentID == id);
         }
     }
 }
